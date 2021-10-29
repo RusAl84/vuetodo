facddbf1aec06a60734002dc216b8f83a18419ab
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-10-29 15:20:30 +0300

    add text field + button

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,128 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!--  <div id="app">-->
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+<!--    <router-view/>-->
+<!--  </div>-->
+  <v-app>
+    <v-navigation-drawer app permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item @click="$router.push('/')">
+          <v-list-item-icon>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="$router.push('/about')">
+          <v-list-item-icon>
+            <v-icon>mdi-help-box</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+<!--      &lt;!&ndash; &ndash;&gt;          <div id="nav">-->
+<!--      <router-link to="/">Home</router-link> |-->
+<!--      <router-link to="/about">About</router-link>-->
+<!--    </div>-->
+
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-main>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view/>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <!-- -->
+    </v-footer>
+  </v-app>
+
+
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import router from "@/router";
+// import API from "@/api/api";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  // name: "LeftDrawer",
+  data () {
+    return {
+      // items: [
+      //   { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      //   { title: 'Photos', icon: 'mdi-image' },
+      //   { title: 'About', icon: 'mdi-help-box' },
+      // ],
+      right: null,
+    }
+  },
+  methods: {
+    async onLoginClick() {
+      try {
+        await API.login(this.username, this.password)
+        router.push('/').catch(()=>{})
+      } catch (e) {
+        console.error(e)
+      }
+      // alert(store.state.authName)
+    },
+    async onLogoutClick() {
+      try {
+        await API.logout()
+        router.push('/login').catch(()=>{})
+      } catch (e) {
+        console.error(e)
+      }
+      // alert(store.state.authName)
+    },
+  },
 }
 </script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+<!--    <img alt="Vue logo" src="../assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+
+    <v-text-field v-model="name"
+        label="Regular"
+    ></v-text-field>
+    <v-text-field v-model="priority"
+        label="Regular"
+    ></v-text-field>
+    <v-btn
+        elevation="2"
+    >Добавить</v-btn>.
+    {{name}}
+    {{priority}}
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  data () {
+    return {
+      // items: [
+      //   { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      //   { title: 'Photos', icon: 'mdi-image' },
+      //   { title: 'About', icon: 'mdi-help-box' },
+      // ],
+      name: "",
+      priority: "",
+    }
+  },
+  components: {
+    // HelloWorld
+  }
+}
+</script>
