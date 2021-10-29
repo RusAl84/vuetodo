5fcec8ecffd02be72728d7ae58ca7bbcf238b3c4
Rusakov Aleksey <34722284+RusAl84@users.noreply.github.com>
2021-10-29 16:51:17 +0300

    fix

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,6 +14,17 @@
     >Добавить</v-btn>.
     {{name}}
     {{priority}}
+    {{lastID}}
+    <!-- Панель сообщений -->
+    <v-card-text>
+      <div class="messages-view">
+        <div v-for="(todo, i) in toDoS" :key="i">
+          <span >{{ todo.name }}</span>
+          <span>{{ todo.priority }}</span>
+
+        </div>
+      </div>
+    </v-card-text>
   </div>
 </template>
 
@@ -23,17 +34,36 @@ import  API from "@/api/api.js"
 import router from "@/router";
 
 export default {
+  // Модель данных страницы
   name: 'Home',
   data () {
     return {
-      // items: [
-      //   { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-      //   { title: 'Photos', icon: 'mdi-image' },
-      //   { title: 'About', icon: 'mdi-help-box' },
-      // ],
+      toDoS: [],
       name: 'test',
       priority: '0',
+      intervalCtx: null,
+      lastID: 0,
     }
+  },
+
+  // Хук который сработает когда страница создасться
+  mounted() {
+
+    this.intervalCtx = setInterval(async () => {
+      try {
+        const msg = await API.getToDo(this.lastID)
+        if ( 'name' in msg) {
+          this.toDoS.push(msg)
+          this.lastID++
+        }
+        else
+        {
+          console.log(this.lastID)
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }, 100)
   },
   methods: {
     async onClick() {
@@ -45,15 +75,10 @@ export default {
       }
       // alert(store.state.authName)
     },
-    async onLogoutClick() {
-      try {
-        await API.logout()
-        router.push('/login').catch(()=>{})
-      } catch (e) {
-        console.error(e)
-      }
-      // alert(store.state.authName)
-    },
+  },
+  // После уничтожения компонента
+  destroyed() {
+    clearInterval(this.intervalCtx)
   },
   components: {
     // HelloWorld
